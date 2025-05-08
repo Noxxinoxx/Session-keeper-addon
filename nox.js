9087b329a022f19da9d0ed18460580ae5758f46c
Nox <nooaanderssondator@gmail.com>
2025-05-08 12:36:33 +0200

    no ide what changed but yeah

--- nox.js
+++ nox.js
@@ -2,35 +2,30 @@
 const toggleButton = document.getElementById('toggleTabs');
 const tabsList = document.getElementById("content");
 toggleButton.addEventListener('click', async () => {
-  
-    // Load tabs
-    let tabs = await browser.tabs.query({}); 
-    for (let tab of tabs) {
-      let p = document.createElement('p');
-      p.textContent = tab.url;
-      tabsList.appendChild(p);
-    }
- 
-    var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+  // Load tabs
+  let tabs = await browser.tabs.query({});
+  for (let tab of tabs) {
+    let p = document.createElement('p');
+    p.textContent = tab.url;
+    tabsList.appendChild(p);
+  }
 
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
 
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
 
 });
-
-
-
 
 
